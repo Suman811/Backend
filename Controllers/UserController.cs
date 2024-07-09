e78a0df78b6d32ec1e4ba42881a4f4e953bcff3f
Suman811 <sumandeepkaur811@gmail.com>
2024-07-09 15:37:48 +0530

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NewBackend.Application.IService;
+using NewBackend.Domain.Entity;
+
+namespace NewBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+
+        }
+
+        [HttpGet("GetDetails")]
+        public async Task<IActionResult> GetDetails()
+        {
+            return Ok(await _userService.GetDetails());
+        }
+
+        [HttpPost("AddUser")]
+         public async Task<IActionResult> CreateSignup(UserModel userModel)
+        {
+            return Ok(await _userService.CreateSignup(userModel));
+        }
+
+
+        }
+
+    } 
